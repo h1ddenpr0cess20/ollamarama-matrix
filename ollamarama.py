58e15ca54381389f0d5476ba5778dd779122e839
Dustin <h1ddenpr0cess2085@gmail.com>
2023-12-17 14:31:29 -0500

    switched model to a more efficient quantization level

--- ollamarama.py
+++ ollamarama.py
@@ -30,7 +30,7 @@ class ollamarama:
         self.prompt = ("you are ", ". speak in the first person and never break character.")
 
         #set model, this one works best in my tests with the hardware i have, but you can try others
-        self.model = "ollama/zephyr:7b-beta-q8_0"
+        self.model = "ollama/zephyr:7b-beta-q6_K"
     
         
     # get the display name for a user
@@ -111,7 +111,7 @@ class ollamarama:
             except Exception as e: 
                 print(e)
             #Shrink history list for token size management 
-            if len(self.messages[channel][sender]) > 20:
+            if len(self.messages[channel][sender]) > 30:
                 del self.messages[channel][sender][1:3]  #delete the first set of question and answers 
 
     # change the personality of the bot
